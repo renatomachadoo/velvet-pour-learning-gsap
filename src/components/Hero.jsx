e08c2448d38647fe56e6d6f170c9476ba5a2b280
Renato Machado <renatomachado176@gmail.com>
2025-09-30 15:34:59 +0100

    feat: Hero section

---
+++ src/components/Hero.jsx
@@ -0,0 +1,97 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { SplitText } from "gsap/all";
+
+export function Hero() {
+  useGSAP(() => {
+    const heroSplit = new SplitText(".title", {
+      type: "chars, words",
+    });
+
+    const paragraphSplit = new SplitText(".subtitle", {
+      type: "lines",
+    });
+
+    heroSplit.chars.forEach((char) => {
+      char.classList.add("text-gradient");
+    });
+
+    gsap.from(heroSplit.chars, {
+      yPercent: 100,
+      duration: 1.8,
+      ease: "expo.out",
+      stagger: 0.06,
+    });
+
+    gsap.from(paragraphSplit.lines, {
+      opacity: 0,
+      yPercent: 100,
+      duration: 1.8,
+      ease: "expo.out",
+      stagger: 0.06,
+      delay: 1,
+    });
+
+    gsap
+      .timeline({
+        scrollTrigger: {
+          trigger: "#hero",
+          start: "top top",
+          end: "bottom top",
+          scrub: true,
+        },
+      })
+      .to(
+        ".right-leaf",
+        {
+          y: 200,
+        },
+        0
+      )
+      .to(
+        ".left-leaf",
+        {
+          y: -200,
+        },
+        0
+      );
+  }, []);
+
+  return (
+    <section className="noisy" id="hero">
+      <h1 className="title">MOJITO</h1>
+
+      <img
+        alt="left-leaf"
+        className="left-leaf"
+        src="/images/hero-left-leaf.png"
+      />
+
+      <img
+        alt="right-leaf"
+        className="right-leaf"
+        src="/images/hero-right-leaf.png"
+      />
+
+      <div className="body">
+        <div className="content">
+          <div className="hidden space-y-5 md:block">
+            <p>Cool. Crisp. Classic.</p>
+            <p className="subtitle">
+              Sip the Spirit <br /> of Summer
+            </p>
+          </div>
+
+          <div className="view-cocktails">
+            <p className="subtitle">
+              Every cocktail we serve is a reflection of our obsession with
+              detail — from the first muddle to the final garnish. That care is
+              what turns a simple drink into something truly memorable.{" "}
+            </p>
+            <a href="#cocktails">View Cocktails</a>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
